0525c4012a849a37b8356fdceadae98fd6d49333
Sagar Khatri <dev.sagarkhatri@gmail.com>
2022-09-22 05:13:55 +0530

    Initial SideBar Component created

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import logo from "./logo.svg";
 import "./App.css";
 import Background from "./components/atoms/background/Background";
 import Header from "./components/atoms/header/Header";
+import Sidebar from "./components/atoms/sidebar/Sidebar";
 
 function App() {
   return (
     <>
       <Background isgradient isCircle />
       <Header />
+      <div className="row">
+        <div className="col-lg-1 col-md-1 col-sm-1">
+          <Sidebar />
+        </div>
+        <div className="col"></div>
+      </div>
     </>
   );
 }
